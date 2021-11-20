2e0a7de2a7457785963eef14bd759d0c36c47c2b
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2021-11-21 00:42:38 +0900

    [update] MonthlyAssetsを関数化

--- moneyforward_monthly_assets.py
+++ moneyforward_monthly_assets.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*-
 
+import time
 import datetime
 from pathlib import Path
 import moneyforward
@@ -8,13 +9,20 @@ import chromedriver
 
 download_path = Path.home().joinpath('Documents/assets')
 
-if __name__ == '__main__':
+
+def downloadMonthlyAssets(year, month):
     print('connecting to browser...')
     driver = chromedriver.open(str(download_path))
     moneyforward.login(driver)
 
-    dt = datetime.datetime.now()
-    date = '{}-{:02d}-01'.format(dt.year, dt.month - 2)
+    date = '{}-{:02d}-01'.format(year, month)
     driver.get(
         'https://moneyforward.com/bs/history/list/{}/monthly/csv'.format(date))
+    time.sleep(5)
     driver.quit()
+    return
+
+
+if __name__ == '__main__':
+    dt = datetime.datetime.now()
+    downloadMonthlyAssets(dt.year, dt.month - 2)
