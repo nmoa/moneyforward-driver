53c5cc584098998eec38b9535217242b8592ffa5
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2022-10-15 23:48:34 +0900

    try-catch追加

--- moneyforward_monthly_assets.py
+++ moneyforward_monthly_assets.py
@@ -14,10 +14,15 @@ def downloadMonthlyAssets(year, month):
     print('connecting to browser...')
     driver = chromedriver.open(str(download_path))
     moneyforward.login(driver)
-
+    print('Downloadding asset data for {}/{}.'.format(year, month))
     date = '{}-{:02d}-01'.format(year, month)
-    driver.get(
-        'https://moneyforward.com/bs/history/list/{}/monthly/csv'.format(date))
+    try:
+        driver.get(
+            'https://moneyforward.com/bs/history/list/{}/monthly/csv'.format(date))
+    except Exception as e:
+        print(e)
+    else:
+        print('Download completed.')
     time.sleep(5)
     driver.quit()
     return
