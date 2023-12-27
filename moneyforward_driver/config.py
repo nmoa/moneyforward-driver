140bd780be0859ed18d168345228bd21a601b58d
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2023-12-27 20:49:24 +0900

    fix: python-dotenvがない場合importしないように設定

--- moneyforward_driver/config.py
+++ moneyforward_driver/config.py
@@ -1,7 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env file
+try:
+    from dotenv import load_dotenv
+    load_dotenv()  # Load environment variables from .env file
+except ImportError:
+    pass
 
 MF_EMAIL = os.getenv('MF_EMAIL')
 MF_PASSWORD = os.getenv('MF_PASSWORD')
